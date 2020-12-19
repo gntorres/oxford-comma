7ab1848ee29ad7bf90d034661cdc1d9ef33add70
gntorres <gntorres421@gmail.com>
2020-12-18 21:47:39 -0600

    commit tree 6ffbca51cd9635f0998142305f08942185621411

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        "#{array.first} and #{array.last}"
+    elsif array.length > 2
+       something = array.pop
+       array << "and #{something}"
+       array.join (", ")
+    else
+        array.first
+    end
 end
